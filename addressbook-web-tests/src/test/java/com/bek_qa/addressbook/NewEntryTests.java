f0a122a32567fa5760dccdfdd24a65bb3f208470
Bek Kenchenbayev <iskk@yahoo.com>
2017-04-18 21:01:19 -0400

    Finished homework #4

--- addressbook-web-tests/src/test/java/com/bek_qa/addressbook/NewEntryTests.java
+++ addressbook-web-tests/src/test/java/com/bek_qa/addressbook/NewEntryTests.java
@@ -3,16 +3,11 @@ package com.bek_qa.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class NewEntryTests {
     FirefoxDriver wd;
@@ -22,52 +17,61 @@ public class NewEntryTests {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret");
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.id("LoginForm")).click();
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username);
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
     @Test
     public void NewEntryTests() {
-        wd.findElement(By.linkText("add new")).click();
+        clickAddNewEntry();
+        enterMyInfo(new ContactData("Bek", "Ken", "2872 E.Broad St", "2872 E.Broad St\nColumbus, OH 43204", "(614)300-0733", "iskk@yahoo.com"));
+        saveEnteredInfo();
+    }
+
+    private void saveEnteredInfo() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void enterMyInfo(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys("Bek");
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getfName());
         wd.findElement(By.name("middlename")).click();
-        wd.findElement(By.name("middlename")).sendKeys("\\9");
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys("Ken");
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getlName());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("2872 E.Broad St");
+        wd.findElement(By.name("address")).sendKeys(contactData.getStreet());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys("2872 E.Broad St\nColumbus, OH 43204");
+        wd.findElement(By.name("address")).sendKeys(contactData.getCityStZip());
         wd.findElement(By.name("home")).click();
-        wd.findElement(By.name("home")).sendKeys("\\9");
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys("(614)300-0733");
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getCellNumber());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys("iskk@yahoo.com");
-        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-        wd.findElement(By.linkText("Logout")).click();
+        wd.findElement(By.name("email")).sendKeys(contactData.geteMail());
     }
-    
+
+    private void clickAddNewEntry() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
     @AfterMethod
     public void tearDown() {
-        wd.quit();
+        //wd.quit();
     }
     
     public static boolean isAlertPresent(FirefoxDriver wd) {
