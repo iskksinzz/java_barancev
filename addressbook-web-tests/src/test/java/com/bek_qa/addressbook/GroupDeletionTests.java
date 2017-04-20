a885d6ceb2e3c041284328e39a9cc1c110946776
Bek Kenchenbayev <iskk@yahoo.com>
2017-04-20 19:12:06 -0400

    Video 8: cleaned GroupDeletedTests() in TestBase

--- addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupDeletionTests.java
@@ -14,45 +14,23 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
-public class GroupDeletionTests {
-    FirefoxDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
+public class GroupDeletionTests extends TestBase {
+
     @Test
     public void GroupDeletionTests() {
-        wd.get("http://localhost/addressbook/");
-        wd.findElement(By.id("LoginForm")).click();
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
+        goToGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+    private void deleteSelectedGroups() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
+    }
+
+    private void selectGroup() {
         if (!wd.findElement(By.xpath("//div[@id='content']/form/span[3]/input")).isSelected()) {
             wd.findElement(By.xpath("//div[@id='content']/form/span[3]/input")).click();
-        }
-        wd.findElement(By.xpath("//div[@id='content']/form/input[5]")).click();
-        wd.findElement(By.linkText("group page")).click();
-    }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
-    }
-    
-    public static boolean isAlertPresent(FirefoxDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
         }
     }
 }
