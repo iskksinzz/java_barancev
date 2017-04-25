4721872dec25aa525d38c1b0b33a2173d70d9661
Bek Kenchenbayev <iskk@yahoo.com>
2017-04-24 21:26:33 -0400

    Video 7. Refactor  login(), so this method takes String username and String password parameters

--- addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupCreationTest.java
@@ -3,16 +3,11 @@ package com.bek_qa.addressbook;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTest {
     FirefoxDriver wd;
@@ -22,16 +17,16 @@ public class GroupCreationTest {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
-        login();
+        login("admin", "secret"); //used to be login() before refactoring
     }
 
-    private void login() {
+    private void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
+        wd.findElement(By.name("user")).sendKeys(username); //used to be sendKeys("admin")
         wd.findElement(By.name("pass")).click();
         wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
+        wd.findElement(By.name("pass")).sendKeys(password);//used to be sendKeys("secret")
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
 
@@ -39,28 +34,28 @@ public class GroupCreationTest {
     public void testGroupCreation() {
         gotoGroupPage();
         initGroupCreation();
-        fillGroupForm();
+        fillGroupForm("group name text", "group header test", "group footer test");
         submitGroupCreation();
         returnToGroupPage();
     }
 
-    //Helper methods refactored from Selenium generated code
+    //Helper methods refactored from Selenium generated code inside testGroupCreation()
     private void gotoGroupPage() {
         wd.findElement(By.linkText("groups")).click();
     }
     private void initGroupCreation() {
         wd.findElement(By.name("new")).click();
     }
-    private void fillGroupForm() {
+    private void fillGroupForm(String groupName, String groupHeader, String groupFooter) { //used to be fillGroupForm()
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("group name text");
+        wd.findElement(By.name("group_name")).sendKeys(groupName);//used to be sendKeys("group name text") before refactoring
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("group header test");
+        wd.findElement(By.name("group_header")).sendKeys(groupHeader);//used to be ("group header test")
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("group footer test");
+        wd.findElement(By.name("group_footer")).sendKeys(groupFooter);//used to be ("group footer test")
     }
     private void submitGroupCreation() {
         wd.findElement(By.name("submit")).click();
