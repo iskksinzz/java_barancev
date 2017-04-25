59095cb438f0110582928e4a3af2b3159090b08b
Bek Kenchenbayev <iskk@yahoo.com>
2017-04-24 21:07:13 -0400

    Video 7: Refactored Selenium generated code for login in @BeforeMethod into login()

--- addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/com/bek_qa/addressbook/GroupCreationTest.java
@@ -22,6 +22,10 @@ public class GroupCreationTest {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
+        login();
+    }
+
+    private void login() {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -30,7 +34,7 @@ public class GroupCreationTest {
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
     }
-    
+
     @Test
     public void testGroupCreation() {
         wd.findElement(By.linkText("groups")).click();
