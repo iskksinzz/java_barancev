ae7c415dadab7b99032d8160bb62da33a2d70eba
Bek Kenchenbayev <iskk@yahoo.com>
2017-05-01 20:06:07 -0400

    Video 3.9: Finished

--- addressbook-web-tests/src/test/java/com/bek_qa/addressbook/appManager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/com/bek_qa/addressbook/appManager/NavigationHelper.java
@@ -1,21 +1,41 @@
 package com.bek_qa.addressbook.appManager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Created by Bek on 4/24/2017.
  */
-public class NavigationHelper {
+public class NavigationHelper extends HelperBase {
     private WebDriver wd; //This object is used by all test classes
 
     public NavigationHelper(WebDriver wd) {
-        this.wd =wd;
+        super(wd);
     }
 
-    //Helper methods refactored from Selenium generated code inside testGroupCreation()
     public void gotoGroupPage() {
-         wd.findElement(By.linkText("groups")).click();
+        if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))){
+            return;
+        }
+         click(By.linkText("groups"));
+    }
+    public void gotoHomePage(){
+        if(isElementPresent(By.id("maintable"))){
+            return;
+        }
+        click(By.linkText("home"));
+    }
+
+    private boolean isElementPresent(By locator) {
+        try{
+            wd.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex){
+            return false;
+        }
     }
 }
